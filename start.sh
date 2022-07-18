803f33db7d9810b55239f55e73e26eb7c7f68c23
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2022-07-18 13:07:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,3 @@ echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
 ngrok tcp 22 --log=stdout > ngrok.log
-ngrok tcp 8888 --log=stdout > ngrok1.log
-ngrok tcp 80 --log=stdout > ngrok2.log
-ngrok tcp 443 --log=stdout > ngrok3.log
